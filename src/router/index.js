d711509ecfa85a32b54aaa5b030a54718f4de90e
GermanVelandia <german.velandia@swissaxis.ch>
2022-11-03 12:11:46 -0500

    Board

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/views/Home'
+import Board from '@/components/Board'
 Vue.use(Router)
 
 export default new Router({
@@ -8,6 +9,12 @@ export default new Router({
     {
       path: '/',
       component: Home
+    },
+    { 
+      path: '/board/:id', 
+      name: 'board', 
+      component: Board, 
+      props: true 
     }
   ]
 })
