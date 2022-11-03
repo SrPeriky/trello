f77d93a7b390265b0c464c760be51580eac56812
GermanVelandia <german.velandia@swissaxis.ch>
2022-11-03 14:17:46 -0500

    taskslist

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -1,49 +1,66 @@
 <template>
     <section>
         <header>
-            <h3>{{name}}</h3>
+            <h3>{{ name }}</h3>
         </header>
-        <div>.</div>
+        <task-list :listId="listId" :tasks="tasksList">
+        </task-list>
     </section>
 </template>
 
 <script>
+import TasksList from './TasksList'
 export default {
     name: 'Column',
+    comments: {TasksList},
     props: {
         listId: String,
         name: String,
+    },
+    data () {
+        return {
+            taskslist: [
+                {id: 1, title: 'T -1 ', completed: false},
+                {id: 2, title: 'T - 2', completed: false},
+                {id: 3, title: 'T - 3', completed: false},
+            ]
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-  section {
+section {
     box-sizing: border-box;
     background-color: #eceff1;
     border-radius: 3px;
-    box-shadow: 0 0 0 0.5px rgba(49,49,93,.03),
-      0 2px 5px 0 rgba(49,49,93,.1),
-      0 1px 2px 0 rgba(0,0,0,.08);
+    box-shadow: 0 0 0 0.5px rgba(49, 49, 93, .03),
+        0 2px 5px 0 rgba(49, 49, 93, .1),
+        0 1px 2px 0 rgba(0, 0, 0, .08);
     margin: 1rem;
     padding: 1rem;
     width: 100%;
+
     @media screen and (min-width: 600px) {
-      width: calc(50% - 2rem - 2px);
+        width: calc(50% - 2rem - 2px);
     }
+
     @media screen and (min-width: 1024px) {
-      width: calc(33% - 2rem - 2px);
+        width: calc(33% - 2rem - 2px);
     }
+
     @media screen and (min-width: 1200px) {
-      width: calc(25% - 2rem - 2px);
+        width: calc(25% - 2rem - 2px);
     }
-  }
-  header {
+}
+
+header {
     color: #37474f;
     margin: 0;
     padding-bottom: 1rem;
-  }
-  h3 {
+}
+
+h3 {
     margin: 0;
-  }
+}
 </style>
