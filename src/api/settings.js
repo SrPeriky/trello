8b64d16d20ae5295524c1afc193c1cd640911e1a
GermanVelandia <german.velandia@swissaxis.ch>
2022-11-03 15:07:34 -0500

    seting api

---
+++ src/api/settings.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: 'AIzaSyB7hmzVtoJt6M_AMAgtbaalaEIXPOf4ERO', 
+    authDomain: 'vue-trello.firebaseapp.com', 
+    databaseURL: 'https://vue-trello.firebaseio.com', 
+    projectId: 'vue-trello',
+    storageBucket: '',
+    messagingSenderId: '28203134008'
+}
